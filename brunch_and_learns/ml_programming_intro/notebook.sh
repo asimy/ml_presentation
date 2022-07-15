af3cd574b67b6399d1d140ae55612a30493343ed
Glen Aultman-Bettridge <glenab@koansolutions.net>
2022-07-15 07:05:04 -0600

    Now without copy paste error

--- brunch_and_learns/ml_programming_intro/notebook.sh
+++ brunch_and_learns/ml_programming_intro/notebook.sh
@@ -2,7 +2,7 @@
 mkdir -p $HOME/machine_learning/notebooks
 
 # Grab the latest version of the anaconda image (yes, this is not best practice, but it's easy)
-docker pull ~/Documents/ML\ Presentation/brunch_and_learns/ml_programming_intro/
+docker pull continuumio/anaconda3
 
 # Run Jupyter notebook in the Anaconda environment making it available on the host machine
 docker run -i -t -v $HOME/machine_learning/notebooks:/opt/notebooks -p 8888:8888 continuumio/anaconda3 /bin/bash -c "\
